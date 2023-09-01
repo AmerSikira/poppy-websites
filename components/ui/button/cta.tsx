086ae8d35b761db05b3fa3589e8b99e927ece431
Amer Sikira <webinuse@gmail.com>
2023-09-01 11:38:11 +0200

    Fixed bug on text alignement for mobile button

--- components/ui/button/cta.tsx
+++ components/ui/button/cta.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Cta = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<Link href="/" className="rounded-md bg-black">
+		<Link href="/" className="rounded-md bg-black text-center">
 			<span
 				className="block -translate-x-2 -translate-y-2 rounded-md border-2 border-black bg-red-500 p-4 text-2xl  hover:-translate-y-3 
     active:translate-x-0 active:translate-y-0
