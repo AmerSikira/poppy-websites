7e2e9187a628f752653740b67bce95211ea64287
Amer Sikira <webinuse@gmail.com>
2023-09-08 14:42:05 +0200

    Added default CTA action

--- components/ui/button/cta.tsx
+++ components/ui/button/cta.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link";
 import React from "react";
 
-const Cta = ({ children }: { children: React.ReactNode }) => {
+const Cta = ({
+	children,
+	href = "mailto:hallo@poppywebsites.be",
+}: {
+	children: React.ReactNode;
+	href?: string;
+}) => {
 	return (
 		<Link
-			href="/"
+			href={href}
 			className="rounded-md bg-red-500 hover:bg-red-600 shadow-3xl text-center block-translate-y-2 border-2 border-black p-4 text-2xl  hover:translate-y-3 
      active:translate-y-0
     transition-all"
