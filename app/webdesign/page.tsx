0e1c74951f6637605ed154fcb5118733172ea422
Amer Sikira <webinuse@gmail.com>
2023-09-11 16:28:34 +0200

    Fixed metadata bug for Webdesign page

--- app/webdesign/page.tsx
+++ app/webdesign/page.tsx
@@ -3,6 +3,16 @@ import Steps from "@/components/homepage/steps";
 import Testimonials from "@/components/homepage/testimonials";
 import Features from "@/components/webdesign/features";
 import Hero from "@/components/webdesign/hero";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Poppy Websites - Webdesign",
+	description: "Wondermooie websites. Toffe teksten. Contente klanten.",
+	openGraph: {
+		title: "Poppy Websites - Webdesign",
+		description: "Wondermooie websites. Toffe teksten. Contente klanten.",
+	},
+};
 
 export default function WebDesign() {
 	return (
