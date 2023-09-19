badbd3f3928ff84e813eeb0ae87e0bb62592214e
Amer Sikira <webinuse@gmail.com>
2023-09-19 19:09:02 +0200

    Changed cookie banner text to dutch

--- components/analytics/cookie-banner.tsx
+++ components/analytics/cookie-banner.tsx
@@ -32,33 +32,34 @@ export default function CookieBanner() {
 	}, [cookieConsent]);
 	return (
 		<div
-			className={`my-10 mx-auto max-w-max md:max-w-screen-sm
+			className={`my-10 mx-auto container
                         fixed bottom-0 left-0 right-0 
                         ${
 							cookieConsent != null ? "hidden" : "flex"
 						}  px-3 md:px-4 py-3 justify-between items-center flex-col sm:flex-row gap-4  
-                         bg-gray-900 rounded-lg shadow`}
+                         bg-gray-900 rounded-lg shadow z-[999]`}
 		>
 			<div className="text-center">
 				<P className="text-white text-bold">
-					We use{" "}
-					<CustomLink href="/privacy-policy">cookies</CustomLink> on
-					our site.
+					Deze website maakt gebruik van{" "}
+					<CustomLink href="/privacy-policy">cookies</CustomLink>.
+					(Niet die van onze naamgenoten uit Beselare - wel digitale
+					cookies om onze website-ervaring te verbeteren)
 				</P>
 			</div>
 
 			<div className="flex gap-2">
 				<button
-					className="px-5 py-2 text-gray-300 rounded-md border-gray-900"
+					className="px-5 py-2 whitespace-nowrap text-gray-300 rounded-md border-gray-900"
 					onClick={() => setCookieConsent(false)}
 				>
-					Decline
+					Alles weigeren
 				</button>
 				<button
-					className="bg-red-100 px-5 py-2 text-black rounded-lg"
+					className="bg-red-100 whitespace-nowrap px-5 py-2 text-black rounded-lg"
 					onClick={() => setCookieConsent(true)}
 				>
-					Allow Cookies
+					Cookies toestaan
 				</button>
 			</div>
 		</div>
